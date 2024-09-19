6de827d4ebcb6ac22d5e90fd4ccfcf752e5df138
Rtollinchi <rtollinchi23@gmail.com>
2024-09-19 15:30:58 -0400

    add User.Create passing test

--- index.test.js
+++ index.test.js
@@ -1,23 +1,23 @@
 const { Comment, Like, Post, Profile, User } = require("./index");
-const { db } = require('./db/connection.js');
+const { db } = require("./db/connection.js");
 
-describe('Social Sequelzie Test', () => {
-    /**
-     * Runs the code prior to all tests
-     */
-    beforeAll(async () => {
-        // the 'sync' method will create tables based on the model class
-        // by setting 'force:true' the tables are recreated each time the test suite is run
-        await sequelize.sync({ force: true });
-    })
+describe("Social Sequelzie Test", () => {
+  /**
+   * Runs the code prior to all tests
+   */
+  beforeAll(async () => {
+    // the 'sync' method will create tables based on the model class
+    // by setting 'force:true' the tables are recreated each time the test suite is run
+    await db.sync({ force: true });
+  });
+  // Write your tests here
 
-    // Write your tests here
-    
-    test("replace with your test", function() {
-        expect(true).toBe(true);
-    })
-
-
-
-
-})
+  test("create a user", async () => {
+    const user = await User.create({
+      username: "ctoll10",
+      email: "ctoll@mail.com",
+    });
+    expect(user.username).toBe("ctoll10");
+    expect(user.email).toBe("ctoll@mail.com");
+  });
+});
